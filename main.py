690f97b2ad36e963920ae4155738d81cb1a9e7fd
jainirban <jainirban9571@gmail.com>
2025-02-03 16:58:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from spire.doc import *
-from spire.doc.common import *
+from spire.doc import Document, FileFormat
 import os
-import base64
+from streamlit_pdf_viewer import pdf_viewer
 
 def convert_to_pdf(docx_file):
     # Create word document
@@ -12,7 +11,7 @@ def convert_to_pdf(docx_file):
     document.LoadFromFile(docx_file)
 
     # Save the document to PDF
-    output_path = "output/converted_file.pdf"
+    output_path = "converted_file.pdf"
     document.SaveToFile(output_path, FileFormat.PDF)
     document.Close()
     return output_path
@@ -32,30 +31,22 @@ if uploaded_file:
 
     # Convert the DOCX file to PDF
     pdf_path = convert_to_pdf(temp_path)
-
     st.write("File converted successfully!")
 
-    # Display the PDF file
+    # Read the PDF file as binary data
     with open(pdf_path, "rb") as pdf_file:
         pdf_bytes = pdf_file.read()
+
+        # Use the pdf_viewer to display the PDF
+        pdf_viewer(input=pdf_bytes, width=700)
+
         st.download_button(
             label="Download PDF",
             data=pdf_bytes,
             file_name="converted_file.pdf",
             mime="application/pdf"
         )
-        st.write("Use the below link to view PDF:")
 
-        # pdf_bytes = pdf_file.read()
-        pdf_base64 = base64.b64encode(pdf_bytes).decode('utf-8')
-        # st.write(f"""
-        # <iframe src="data:application/pdf;base64,{pdf_bytes.decode()}" width="700" height="500" type="application/pdf"></iframe>
-        # """, unsafe_allow_html=True)
-        pdf_display = f"""
-        <iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="900" type="application/pdf"></iframe>
-        """
-        st.markdown(pdf_display, unsafe_allow_html=True)
-
-    # Remove the temporary files (optional)
+    # Optionally, remove the temporary files
     os.remove(temp_path)
     os.remove(pdf_path)
